58ff8a3c5c7487769df21f181387633bb3f875a9
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2022-10-12 15:03:32 +0200

    Updates

---
+++ precache-manifest.675bbb1c2dc3664e316664b6a1a2372c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3179a2a870b45c2d5617",
+    "url": "/KamilWawrzynczuk/robofriends2/static/css/main.1241c120.chunk.css"
+  },
+  {
+    "revision": "3179a2a870b45c2d5617",
+    "url": "/KamilWawrzynczuk/robofriends2/static/js/main.5ed19c64.chunk.js"
+  },
+  {
+    "revision": "d1e26a3d482f3d890231",
+    "url": "/KamilWawrzynczuk/robofriends2/static/js/runtime~main.624ccd6c.js"
+  },
+  {
+    "revision": "bcf60d58cfa4baec93d5",
+    "url": "/KamilWawrzynczuk/robofriends2/static/css/2.a36f7cb3.chunk.css"
+  },
+  {
+    "revision": "bcf60d58cfa4baec93d5",
+    "url": "/KamilWawrzynczuk/robofriends2/static/js/2.38e77c2d.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/KamilWawrzynczuk/robofriends2/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7b836ce5e7b172cecca40affe05255e3",
+    "url": "/KamilWawrzynczuk/robofriends2/index.html"
+  }
+];
